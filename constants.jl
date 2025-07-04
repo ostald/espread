d1939ce78b1f9e8d4a1103bd485dd2f27c046c75
Oliver Stalder <ostalder@outlook.com>
2025-07-04 15:50:34 +0200

    add altitude function

--- constants.jl
+++ constants.jl
@@ -13,9 +13,17 @@ re = 6378e3  #m
 )
 
 function E_ev(v_abs)
+    # energy in eV of an electron travelling with |v| = v_abs
     return v_abs^2 * c.me  / (2 * c.qe) 
 end
 
 function v_abs(E_ev)
+    # velocity of an electron with kinetic energy E_ev
     return sqrt(E_ev * c.qe * 2 / c.me)
+end
+
+function altitude(r)
+    # altitude above earth's surface of a particle at coordinates r
+    # with origin at geomagnetic center of earth
+    return norm(r) - c.re
 end
