0de681d97257b64cfe2c658eca1cd2adebff34f2
Oliver Stalder <ostalder@outlook.com>
2025-07-07 15:23:12 +0200

    saving seed

--- setup.jl
+++ setup.jl
@@ -6,7 +6,7 @@ using Dates
 E0 = 50 #eV
 
 # Define number of particles
-N_electrons = 1e6
+N_electrons = 1e2
 
 # starting altitude
 alt0 = 600e3 #m
@@ -16,11 +16,15 @@ lim_pitch_deg = 90 #deg
 lim_pitch = 90/180*pi
 
 # initiate seed
-Random.seed!(E0) # use energy as seed so that runs of the same energy are reproducible
+Random.seed!(Int(E0)) # use energy as seed so that runs of the same energy are reproducible
 
 
 # Results directory
 res_dir = joinpath("results", string(now()))
+mkdir(res_dir)
+open(joinpath(res_dir, "results.txt"), "w") do file
+    write(file, "Seed = $E0\n\n")
+end
 
 
 #geomagnetic location of tromso:
@@ -30,3 +34,13 @@ loc_gmag = loc_gmag_deg ./ 180 * pi
 
 loc_geod_deg = [69.58, 19.23]
 loc_geod = loc_geod_deg ./ 180 * pi
+
+
+
+
+"""
+TO DO
+
+- Double ion not handled correctly yet
+- some tracing?
+"""
