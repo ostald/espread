64c3a3e9b2ade707c1f6d90cd14c219b104712f0
Oliver Stalder <ostalder@outlook.com>
2025-07-10 15:55:16 +0200

    speed up magnetic field

--- magnetic_field.jl
+++ magnetic_field.jl
@@ -1,6 +1,20 @@
 using LinearAlgebra
 
-function dipole_field(p, vm)
+
+function dipole_field_earth(p::Vector{Float64})
+    vm = [0, 0, -8.22e15] # Tm3
+    return dipole_field(p, vm)
+end
+
+
+function dipole_field_earth!(B::Vector{Float64}, p::Vector{Float64})
+    vm = [0, 0, -8.22e15] # Tm3
+    dipole_field!(B, p, vm)
+    nothing
+end
+
+
+function dipole_field_(p, vm)
     # p - point to calculate the magnetic field
     # vm - moment vector for the magnetic field orientation
     #       8.22e15 Tm3 for earth
@@ -16,13 +30,51 @@ function dipole_field(p, vm)
     dipole = [
         3*p[1]^2 - r^2  3*p[1]*p[2]     3*p[1]*p[3];
         3*p[1]*p[2]     3*p[2]^2 - r^2  3*p[2]*p[3];
-        2*p[1]*p[3]     3*p[2]*p[3]     3*p[3]^2 - r^2
+        3*p[1]*p[3]     3*p[2]*p[3]     3*p[3]^2 - r^2
         ]
     B = 1/r^5 * dipole * vm
     return B
 end
 
-function dipole_field_earth(p)
-    vm = [0, 0, -8.22e15] # Tm3
-    return dipole_field(p, vm)
+
+B = Vector{Float64}(undef, 3)
+
+function dipole_field(p::Vector{Float64}, vm::Vector{Float64})
+    #so far the fastest!
+    r = norm(p)
+    B = 1/r^5 * (3* p * dot(p, vm) - vm * r^2) 
+    return B
 end
+
+
+
+
+function dipole_field!(B::Vector{Float64}, p::Vector{Float64}, vm::Vector{Float64})
+    #so far the fastest!
+    r = norm(p)
+    B = 1/r^5 * (3* p * dot(p, vm) - vm * r^2) 
+    nothing
+end
+
+
+"""
+function dipole_field4(p, vm)
+
+    r = norm(p)
+    B = 1/r^5 * (3* p * dot(p, vm)) - vm / r^3 
+
+    return B
+end
+
+function dipole_field2(p, vm)
+    #half as fast :(
+    r = norm(p)
+    dipole = Hermitian([
+        p[1]^2 - r^2  p[1]*p[2]     p[1]*p[3];
+        0             p[2]^2 - r^2  p[2]*p[3];
+        0             0             p[3]^2 - r^2
+        ])
+    B = 1/r^5 * 3 * dipole * vm
+    return B
+end
+"""
