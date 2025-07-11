f989f37e06362bf51c4f71f338e2a3e50ce8d36e
Etienne <etienne.gavazzi@uit.no>
2025-07-11 18:29:13 +0200

    faster

--- constants.jl
+++ constants.jl
@@ -14,10 +14,18 @@ re = 6378e3  #m
 
 function E_ev(v_abs)
     # energy in eV of an electron travelling with |v| = v_abs
-    return v_abs^2 * c.me  / (2 * c.qe) 
+    return v_abs^2 * c.me  / (2 * c.qe)
+end
+function E_ev(v_abs, c)
+    # energy in eV of an electron travelling with |v| = v_abs
+    return v_abs^2 * c.me  / (2 * c.qe)
 end
 
 function v_abs(E_ev)
+    # velocity of an electron with kinetic energy E_ev
+    return sqrt(E_ev * c.qe * 2 / c.me)
+end
+function v_abs(E_ev, c)
     # velocity of an electron with kinetic energy E_ev
     return sqrt(E_ev * c.qe * 2 / c.me)
 end
@@ -27,3 +35,17 @@ function altitude(r)
     # with origin at geomagnetic center of earth
     return norm(r) - c.re
 end
+function altitude(r, c)
+    # altitude above earth's surface of a particle at coordinates r
+    # with origin at geomagnetic center of earth
+    return norm(r) - c.re
+end
+
+
+x = 1e6
+@btime E_ev(x)      # 220ns
+@btime E_ev(x, c)   # 30ns
+@btime v_abs(x)   # 240ms
+@btime v_abs(x, c)   # 35ns
+@btime E_ev(x)   # 30ns
+@btime E_ev(x, c)   # 30ns
