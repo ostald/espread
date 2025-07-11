43bc964c943aa403d668cbf03bea884bdcf5d509
Oliver Stalder <ostalder@outlook.com>
2025-07-11 18:17:15 +0200

    add constants to arguments of functions for type stabilising

--- constants.jl
+++ constants.jl
@@ -10,19 +10,21 @@ qe = 1.6021766208e-19	#C
 # earth radius
 re = 6378e3  #m
 ,
+vm = [0, 0, -8.22e15] # Tm3
+,
 )
 
-function E_ev(v_abs)
+function E_ev(v_abs::Float64, c)
     # energy in eV of an electron travelling with |v| = v_abs
     return v_abs^2 * c.me  / (2 * c.qe) 
 end
 
-function v_abs(E_ev)
+function v_abs(E_ev::Float64, c)
     # velocity of an electron with kinetic energy E_ev
     return sqrt(E_ev * c.qe * 2 / c.me)
 end
 
-function altitude(r)
+function altitude(r::Vector{Float64}, c)
     # altitude above earth's surface of a particle at coordinates r
     # with origin at geomagnetic center of earth
     return norm(r) - c.re
