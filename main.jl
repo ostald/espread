213290a0961cca0d4f227f53694712c4cd01a708
Oliver Stalder <ostalder@outlook.com>
2025-07-12 15:46:42 +0200

    adding arguemnt c to main call

--- main.jl
+++ main.jl
@@ -9,6 +9,7 @@ procs = addprocs(10)
 #f = @spawnat 2  main(4000, 2, alt0, 20, loc_gmag, loc_geod)
 #ff = fetch(f)
 
+"""
 f2  = @spawnat 2  main( 500, N_electrons, alt0, 20, loc_gmag, loc_geod)
 f3  = @spawnat 3  main( 500, N_electrons, alt0, 90, loc_gmag, loc_geod)
 f4  = @spawnat 4  main(1000, N_electrons, alt0, 20, loc_gmag, loc_geod)
@@ -19,13 +20,13 @@ f8  = @spawnat 8  main(4000, N_electrons, alt0, 20, loc_gmag, loc_geod)
 f9  = @spawnat 9  main(4000, N_electrons, alt0, 90, loc_gmag, loc_geod)
 f10 = @spawnat 10 main(8000, N_electrons, alt0, 20, loc_gmag, loc_geod)
 f11 = @spawnat 11 main(8000, N_electrons, alt0, 90, loc_gmag, loc_geod)
+"""
 
-
-i = 2
+i_proc = 2
 for E0 in e_energy
     for pl in pitch_limits_deg
-        @spawnat i  main(E0, N_electrons, alt0, pl, loc_gmag, loc_geod)
-        i = i+1
+        @spawnat i_proc  main(E0, N_electrons, alt0, pl, loc_gmag, loc_geod, c)
+        i_proc = i_proc+1
         break
     end
 end
