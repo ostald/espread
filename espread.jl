ddf78017a49792232a4c0c54e5303f92883d37ac
Oliver <ostalder@outlook.com>
2026-01-21 18:19:00 +0100

    fixing angle distribution

--- espread.jl
+++ espread.jl
@@ -53,7 +53,7 @@ function initialize_primary_electron(E0, loc_gmag, alt0, lim_pitch, c, b_model, 
     #phase = rand()*2*pi         # random phase angle
 
     #pitch = acos.(1 .-rand()*(1-cos(lim_pitch)))
-    pitch = acos.(1 .-rand()* 2* sin(lim_pitch)^2) #higher precision by avoiding subtraction
+    pitch = acos.(1 .-rand()* 2* sin(lim_pitch/2)^2) #higher precision by avoiding subtraction
     phase = rand()*2*pi
 
     """
