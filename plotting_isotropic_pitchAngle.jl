12a9c0dc3cfcc6322a434cf89d1c1601aa9f431e
Oliver <ostalder@outlook.com>
2026-08-17 00:24:25 +0200

    new file for plotting pitch angle experiments from isotropic distribution

--- plotting_isotropic_pitchAngle.jl
+++ plotting_isotropic_pitchAngle.jl
@@ -1,8 +1,8 @@
 using Serialization
 using DataFrames
-using LinearAlgebra
-using CSV
-using ImageFiltering
+#using LinearAlgebra
+#using CSV
+#using ImageFiltering
 
 using MathTeXEngine
 set_texfont_family!(FontFamily("TeXGyreHeros"))
@@ -61,7 +61,7 @@ r = runs[2]
 println(r)
 io = open(joinpath(dir, "hist_pitch_summed", r), "r")
 E0, lim_pitch_deg, seed_value, hmin, hmax, hintervals, his_pitch, ne_pitch_summed = deserialize(io)
-E0, lim_pitch_deg, seed_value, hmin, hmax, hintervals, his_pitch = deserialize(io)
+#E0, lim_pitch_deg, seed_value, hmin, hmax, hintervals, his_pitch = deserialize(io)
 close(io)
 
 #his_pitch.weights = (his_pitch.weights' ./ ne_pitch_summed)'
@@ -79,6 +79,7 @@ pitch_middle = pitch_edges[1:end-1] + diff(pitch_edges)/2
 z_edges = his_pitch.edges[1]
 z_middle = z_edges[1:end-1] + diff(z_edges)/2
 
+#=
 fig, ax, hm = heatmap(rad2deg.(pitch_middle), z_middle./1e3, his_pitch.weights',
     axis = (xlabel = "Pitch Angle [deg]", ylabel = "Height [km]"),
     colorscale = log10,
@@ -87,6 +88,7 @@ fig, ax, hm = heatmap(rad2deg.(pitch_middle), z_middle./1e3, his_pitch.weights',
 Colorbar(fig[1, 2], hm)
 fig
 xlims!(0, 90)
+=#
 
 id = 67
 fig, ax, lin = lines(his_pitch.weights[:, id], z_middle./1e3, label = "$(round(rad2deg(pitch_edges[id]))) - $(round(rad2deg(pitch_edges[id+1])))",
@@ -95,6 +97,7 @@ lines!(ax, his_pitch.weights[:, id-1], z_middle./1e3, label = "$(round(rad2deg(p
 axislegend(ax)
 fig
 
+#=
 fig = Figure()
 ax = Axis(fig[1, 1], xscale = log10, limits = ((1e-4, 1e2), nothing))
 for id in 60:70
@@ -102,14 +105,14 @@ for id in 60:70
 end
 axislegend(ax)
 fig
-
+=#
 
 
 id = 68
 data = dropdims(sum(his_pitch.weights[:, id:end], dims = 2), dims = 2)
-fig, ax, lin = lines(data, z_middle./1e3, label = "Sum $(rad2deg(pitch_edges[id])) deg to 90 deg",
+fig, ax, lin = lines(data, z_middle./1e3, label = "$(round(Int, rad2deg(pitch_edges[id])))° - 90°",
     axis = (xscale = log10, limits = ((1e-5, 1e1), nothing)),)
-lines!(ax, his_pitch.weights[:, id], z_middle./1e3, label = "$(round(rad2deg(pitch_edges[id]))) deg - $(round(rad2deg(pitch_edges[id+1]))) deg",)
+lines!(ax, his_pitch.weights[:, id], z_middle./1e3, label = "$(round(Int, rad2deg(pitch_edges[id])))° - $(round(Int, rad2deg(pitch_edges[id+1])))°",)
 axislegend(ax)
 xlims!(1e-4, 1e0)
 fig
@@ -117,97 +120,3 @@ save(joinpath(dir, "plots", "production_profile_sum90.png"), fig, px_per_unit = 
 
 
 
-
-
-##
-
-dir = "results/r14_pitchAngle_2026-05-07T14:46:40.781/"
-dir_con = readdir(joinpath(dir, "hist_summed"))
-
-dir_con_raw = filter(x-> contains(x, ".hist"), dir_con)
-runs = unique(dir_con_raw)
-
-runs_xyz = filter(x-> contains(x, "xyz"), runs)
-runs_hrp = filter(x-> contains(x, "hrp"), runs)
-
-runs_xyz_4kev = filter(x-> contains(x, "4000.0eV"), runs_xyz)
-runs_xyz_10kev = filter(x-> contains(x, "10000.0eV"), runs_xyz)
-runs_xyz_40kev = filter(x-> contains(x, "40000.0eV"), runs_xyz)
-
-E0 = 0
-
-f = Figure()
-sleep(1)
-ax = Axis(f[1, 1], 
-        xlabel = "Production [m⁻³]",
-        ylabel = "Height [km]",
-        limits = ((1e-6, 1e2), (80, 600)),
-        xscale = log10,
-        #xticks = LogTicks(-4:-2),
-        #title = "Production vs Height isotropic"
-        )
-for r in runs_xyz_10kev[1:4]
-#for r in runs_xyz
-    println(r)
-    io = open(joinpath(dir, "hist_summed", r), "r")
-    E0, lim_pitch_deg, seed_value, hmin, hmax, hintervals, his_xyz = deserialize(io)
-    close(io)
-
-    data = dropdims(sum(his_xyz.weights, dims = (1, 2)), dims = (1, 2))
-    #dA = diff(his_xyz.edges[1])[1]*diff(his_xyz.edges[2])[1]
-    z_edges = his_xyz.edges[3]
-    z_middle = z_edges[1:end-1] + diff(z_edges)/2   
-    lines!(ax, data/1e6, z_middle/1e3, label = "$(round(Int, E0/1000)) keV, $(round(Int, lim_pitch_deg)) deg")
-end
-axislegend(ax)
-xlims!(1e-5, 1e0)
-xlims!(1e-6, 1e-2)
-ylims!(80, 400)
-f
-save(joinpath(dir, "plots", "production_profile_pitch_angle_scan_$(E0)eV.png"), f, px_per_unit = 3.3)
-##
-
-dir = "results/r14_pitchAngle_2026-05-07T14:46:40.781/"
-
-
-draw_plim=string(65)
-f = Figure()
-sleep(1)
-ax = Axis(f[1, 1], 
-        xlabel = "Production [m⁻³]",
-        ylabel = "Height [km]",
-        limits = ((1e-6, 1e3), (80, 600)),
-        xscale = log10,
-        #xticks = LogTicks(-4:-2),
-        #title = "Production vs Height isotropic"
-        )
-r =  "h_hrp_10000.0eV_$draw_plim.0deg_summed.hist"
-r =  "h_pitch_4000.0eV_$draw_plim.0deg_summed.hist"
-println(r)
-io = open(joinpath(dir, "hist_summed", r), "r")
-io = open(joinpath(dir, "hist_pitch_summed", r), "r")
-E0, lim_pitch_deg, seed_value, hmin, hmax, hintervals, his_hrp = deserialize(io)
-close(io)
-
-data = dropdims(sum(his_hrp.weights, dims = (2, 3)), dims = (2, 3))
-h_edges = his_hrp.edges[1]
-h_middle = h_edges[1:end-1] + diff(h_edges)/2   
-lines!(ax, data/1e6, h_middle/1e3, label = "MC $E0 eV, $lim_pitch_deg")
-f
-
-r =  "h_hrp_40000.0eV_$draw_plim.0deg_summed.hist"
-println(r)
-io = open(joinpath(dir, "hist_summed", r), "r")
-E0, lim_pitch_deg, seed_value, hmin, hmax, hintervals, his_hrp = deserialize(io)
-close(io)
-
-data = dropdims(sum(his_hrp.weights, dims = (2, 3)), dims = (2, 3))
-h_edges = his_hrp.edges[1]
-h_middle = h_edges[1:end-1] + diff(h_edges)/2   
-lines!(ax, data/1e6, h_middle/1e3, label = "MC $E0 eV, $lim_pitch_deg")
-#lines(data/1e6, h_middle/1e3, label = "MC $E0 eV", axis = (xscale = log10,),)
-axislegend(ax)
-ylims!(80, 400)
-xlims!(1e-5, 1e0)
-f
-save(joinpath(dir, "plots", "production_profile_pitch_angle_$lim_pitch_deg.png"), f, px_per_unit = 3.3)
